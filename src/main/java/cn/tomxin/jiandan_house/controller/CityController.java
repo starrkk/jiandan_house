a982f92d2a235a1a382b96f66fa563b052d63ce8
陈通兴 <221360@nd.com>
2018-11-22 20:36:04 +0800

    代码优化

--- src/main/java/cn/tomxin/jiandan_house/controller/CityController.java
+++ src/main/java/cn/tomxin/jiandan_house/controller/CityController.java
@@ -3,16 +3,17 @@ package cn.tomxin.jiandan_house.controller;
 import cn.tomxin.jiandan_house.entity.City;
 import cn.tomxin.jiandan_house.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
- * city controller
- *
- * @author tomxin
- * @version 1.0
- */
+  * @Description: 城市相关
+  * @author: tomxin(221360)
+  * @date: 2018/11/22 19:18
+  * @Version 1.0
+ **/
 @RestController
 @RequestMapping(value = "/v0.1/city")
 public class CityController {
@@ -26,8 +27,8 @@ public class CityController {
      * @return
      */
     @GetMapping(value = "/list")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public List<City> list(){
-
         return cityService.list();
     }
 
